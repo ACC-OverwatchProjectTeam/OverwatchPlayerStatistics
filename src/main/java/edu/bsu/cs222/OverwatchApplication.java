8baf5ab444ee799fefb8efde329f9b6a84a7db04
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-10-14 17:36:12 -0400

    Add JavaFX capabilities and OverwatchApplication class

---
+++ src/main/java/edu/bsu/cs222/OverwatchApplication.java
@@ -0,0 +1,11 @@
+package edu.bsu.cs222;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OverwatchApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
