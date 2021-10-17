17248e24c12b161cf6c9c5129f598a66375b928e
Camden Craighead <33268448+dvds12@users.noreply.github.com>
2021-10-16 23:26:37 -0400

    Add test and method to format rank as rank name instead of rank points.

--- src/main/java/edu/bsu/cs222/DataFormatter.java
+++ src/main/java/edu/bsu/cs222/DataFormatter.java
@@ -1,4 +1,29 @@
 package edu.bsu.cs222;
 
 public class DataFormatter {
+
+    public String formatRank(String s) {
+        int rank = Integer.parseInt(s);
+        if (rank > 3999){
+            return "Grandmaster";
+        }
+        else if (rank > 3499){
+            return "Master";
+        }
+        else if (rank > 2999){
+            return "Diamond";
+        }
+        else if (rank > 2499){
+            return "Platinum";
+        }
+        else if (rank > 1999){
+            return "Gold";
+        }
+        else if (rank > 1499){
+            return "Silver";
+        }
+        else{
+            return "Bronze";
+        }
+    }
 }
--- src/test/java/edu/bsu/cs222/DataFormatterTest.java
+++ src/test/java/edu/bsu/cs222/DataFormatterTest.java
@@ -1,4 +1,12 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DataFormatterTest {
+    @Test
+    public void testFormatRank(){
+        DataFormatter dataFormatter = new DataFormatter();
+        Assertions.assertEquals("Platinum", dataFormatter.formatRank("2785"));
+    }
 }
