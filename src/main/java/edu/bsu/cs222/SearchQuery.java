c48f1c810067e83e17961f9bbc964dd9b52869bf
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-10-19 16:08:02 -0400

    Add test and method for SearchQuery that creates a URL

--- src/main/java/edu/bsu/cs222/SearchQuery.java
+++ src/main/java/edu/bsu/cs222/SearchQuery.java
@@ -1,4 +1,7 @@
 package edu.bsu.cs222;
 
 public class SearchQuery {
+    public String createURLFromSearchQuery(String searchQuery) {
+        return String.format("https://ow-api.com/v1/stats/pc/us/%s/complete", searchQuery.replace('#', '-'));
+    }
 }
--- src/test/java/edu/bsu/cs222/SearchQueryTest.java
+++ src/test/java/edu/bsu/cs222/SearchQueryTest.java
@@ -1,4 +1,13 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SearchQueryTest {
+    @Test
+    public void testCreateURLFromSearchQuery() {
+        SearchQuery searchQuery = new SearchQuery();
+        Assertions.assertEquals("https://ow-api.com/v1/stats/pc/us/Loupine-1170/complete",
+                searchQuery.createURLFromSearchQuery("Loupine#1170"));
+    }
 }
