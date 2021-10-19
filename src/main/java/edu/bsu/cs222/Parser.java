64924ca4af46262de39868015c7f53457ce7eba9
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-10-19 17:16:49 -0400

    Refactor LoupineData resource and ParserTest and Parser methods
    The LoupineData resource is now a text file instead of a json file, and the ParserTest and Parser methods now use strings as their input data instead of InputStreams.

--- src/main/java/edu/bsu/cs222/Parser.java
+++ src/main/java/edu/bsu/cs222/Parser.java
@@ -4,16 +4,15 @@ import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class Parser {
-    public String accessUsername(InputStream profileDataStream) throws IOException {
+    public String accessUsername(String profileDataStream) {
         JsonPath path = JsonPath.compile("$..name");
         JSONArray playerName = path.read(profileDataStream);
         return playerName.get(0).toString();
     }
 
-    public String accessSkillRating(InputStream profileDataStream) throws IOException {
+    public String accessSkillRating(String profileDataStream) {
         JsonPath path = JsonPath.compile("$..rating");
         JSONArray skillRating = path.read(profileDataStream);
         return String.valueOf(skillRating.get(0));
--- src/test/java/edu/bsu/cs222/ParserTest.java
+++ src/test/java/edu/bsu/cs222/ParserTest.java
@@ -1,22 +1,26 @@
 package edu.bsu.cs222;
 
+import com.google.common.io.Resources;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class ParserTest {
+    private final String testData = Resources.toString(Resources.getResource("LoupineData.txt"), StandardCharsets.UTF_8);
     private final Parser parser = new Parser();
+
+    public ParserTest() throws IOException {
+    }
+
     @Test
-    public void testAccessUsername() throws IOException {
-        InputStream testData = Thread.currentThread().getContextClassLoader().getResourceAsStream("LoupineData.json");
+    public void testAccessUsername() {
         Assertions.assertEquals("Loupine#1170", parser.accessUsername(testData));
     }
 
     @Test
-    public void testAccessSkillRating() throws IOException {
-        InputStream testData = Thread.currentThread().getContextClassLoader().getResourceAsStream("LoupineData.json");
+    public void testAccessSkillRating() {
         Assertions.assertEquals("2785", parser.accessSkillRating(testData));
     }
 }
