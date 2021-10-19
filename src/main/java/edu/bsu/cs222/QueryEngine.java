2ff7e8d631bf7809a0672ea68b6feda49eedc99e
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-10-19 16:12:15 -0400

    Add methods to build and send an HTTP Request to Overwatch API
    The HTTP Request methods require a URL as a string to function. They then build and send the request to the API and returns the data as a string.

--- src/main/java/edu/bsu/cs222/QueryEngine.java
+++ src/main/java/edu/bsu/cs222/QueryEngine.java
@@ -1,4 +1,25 @@
 package edu.bsu.cs222;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class QueryEngine {
+    public String sendHTTPRequest(String queryURL) throws URISyntaxException, IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(
+                buildRequest(queryURL), HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    private HttpRequest buildRequest(String queryURL) throws URISyntaxException {
+        return HttpRequest.newBuilder()
+                .uri(new URI(queryURL))
+                .GET()
+                .build();
+    }
 }
+
