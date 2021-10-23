7b3cb54518dadb87aaf5449b8b71f06aaa202f3a
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-10-23 10:32:45 -0400

    Add JavaFx search button and search input line in an HBox

--- src/main/java/edu/bsu/cs222/OverwatchApplication.java
+++ src/main/java/edu/bsu/cs222/OverwatchApplication.java
@@ -1,11 +1,41 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class OverwatchApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setTitle("Overwatch Application");
+        primaryStage.setScene(createScene());
         primaryStage.show();
     }
+
+    private Scene createScene() {
+        HBox searchHBox = createSearchHBox();
+
+        int sceneWidth = 1280;
+        int sceneHeight = 720;
+        return new Scene(searchHBox, sceneWidth,sceneHeight);
+    }
+
+    private HBox createSearchHBox() {
+        TextField searchLine = new TextField("Enter your exact BattleTag");
+        searchLine.setPrefWidth(200);
+        searchLine.setPrefHeight(100);
+
+        Button searchButton = new Button("Search");
+        searchButton.setPrefWidth(200);
+        searchButton.setPrefHeight(100);
+
+        HBox searchHBox = new HBox(searchLine, searchButton);
+        searchHBox.setAlignment(Pos.TOP_CENTER);
+        return searchHBox;
+    }
+
 }
