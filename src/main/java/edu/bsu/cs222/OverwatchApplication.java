26be76b95bd100d8c4099f929f403ab4f011b225
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-10-24 02:27:24 -0400

    Fix bug that caused a URI Exception

--- src/main/java/edu/bsu/cs222/OverwatchApplication.java
+++ src/main/java/edu/bsu/cs222/OverwatchApplication.java
@@ -8,13 +8,20 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class OverwatchApplication extends Application {
+    private final Runnable initializePlayerTask = new OverwatchApplication.InitializePlayerTask();
     private final TextField searchInputField;
     private final Button searchButton;
+    public Player player;
+    public String playerData;
 
     public OverwatchApplication(){
         searchInputField = new TextField("Enter your exact BattleTag");
         searchButton = new Button("Search");
+        searchButton.setOnAction(event -> initializePlayerTask.run());
     }
 
     @Override
@@ -35,5 +42,27 @@ public class OverwatchApplication extends Application {
         HBox searchHBox = new HBox(searchInputField, searchButton);
         searchHBox.setAlignment(Pos.TOP_CENTER);
         return searchHBox;
+    }
+
+    private final class InitializePlayerTask implements Runnable {
+
+        @Override
+        public void run() {
+            SearchQuery searchQuery = new SearchQuery();
+            String playerBattleTag = searchInputField.getText();
+
+            try {
+                OverwatchApplication.this.playerData = searchQuery.createURLFromSearchQuery(playerBattleTag);
+            } catch (URISyntaxException | InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+
+            OverwatchApplication.this.player = new Player.Builder().parserSetup(playerData)
+                    .withPlayerData()
+                    .withPlayerRatingInfo()
+                    .withPlayerLevel()
+                    .withCompetitiveGameData()
+                    .withQuickPlayGameData();
+        }
     }
 }
