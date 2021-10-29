3e92f706ad86b5e535fb2015e3f4b6d46c26a2b4
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-10-28 22:54:40 -0400

    Refactor parser return types to return primitive types

--- src/main/java/edu/bsu/cs222/Parser.java
+++ src/main/java/edu/bsu/cs222/Parser.java
@@ -23,7 +23,7 @@ public class Parser {
         return String.valueOf(playerIcon.get(0));
     }
 
-    public Integer accessSkillRating() {
+    public int accessSkillRating() {
         JsonPath path = JsonPath.compile("$..rating");
         JSONArray skillRating = path.read(dataStream);
         return formatter.convertStringToInteger(String.valueOf(skillRating.get(0)));
@@ -35,37 +35,37 @@ public class Parser {
         return String.valueOf(ratingIcon.get(0));
     }
 
-    public Integer accessLevel() {
+    public int accessLevel() {
         JsonPath path = JsonPath.compile("$..level");
         JSONArray level = path.read(dataStream);
         return formatter.convertStringToInteger(String.valueOf(level.get(0)));
     }
 
-    public Integer accessPrestige() {
+    public int accessPrestige() {
         JsonPath path = JsonPath.compile("$..prestige");
         JSONArray prestige = path.read(dataStream);
         return formatter.convertStringToInteger(String.valueOf(prestige.get(0)));
     }
 
-    public Integer accessTotalQuickPlayGames() {
+    public int accessTotalQuickPlayGames() {
         JsonPath path = JsonPath.compile("$..quickPlayStats.games.played");
         JSONArray totalQuickPlayGames = path.read(dataStream);
         return formatter.convertStringToInteger(String.valueOf(totalQuickPlayGames.get(0)));
     }
 
-    public Integer accessTotalQuickPlayWins() {
+    public int accessTotalQuickPlayWins() {
         JsonPath path = JsonPath.compile("$..quickPlayStats.games.won");
         JSONArray totalQuickPlayWins = path.read(dataStream);
         return formatter.convertStringToInteger(String.valueOf(totalQuickPlayWins.get(0)));
     }
 
-    public Integer accessTotalCompetitiveGames() {
+    public int accessTotalCompetitiveGames() {
         JsonPath path = JsonPath.compile("$..competitiveStats.games.played");
         JSONArray totalCompetitiveGames = path.read(dataStream);
         return formatter.convertStringToInteger(String.valueOf(totalCompetitiveGames.get(0)));
     }
 
-    public Integer accessTotalCompetitiveWins() {
+    public int accessTotalCompetitiveWins() {
         JsonPath path = JsonPath.compile("$..competitiveStats.games.won");
         JSONArray totalCompetitiveWins = path.read(dataStream);
         return formatter.convertStringToInteger(String.valueOf(totalCompetitiveWins.get(0)));
