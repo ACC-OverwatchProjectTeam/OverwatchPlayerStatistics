559444c71310aca52fd7b77fd24e89f332a68978
Corey Sistrunk <csistrunk@bsu.edu>
2021-12-02 14:51:11 -0800

    Use enumerated types to find ranks

--- src/main/java/edu/bsu/cs/Model/DataFormatter.java
+++ src/main/java/edu/bsu/cs/Model/DataFormatter.java
@@ -2,28 +2,55 @@ package edu.bsu.cs.Model;
 
 public class DataFormatter {
 
-    public String formatSkillRatingAsRankName(String skillRating) {
+    public String formatSkillRatingAsRankName (String skillRating) {
         int rank = Integer.parseInt(skillRating);
-        if (rank > 3999){
-            return "Grandmaster";
+        Rank rank1;
+
+        if (rank > 3999) {
+            rank1 = Rank.Grandmaster;
+            return new RankTest(rank1).RankSet();
+        } else if (rank > 3499) {
+            rank1 = Rank.Master;
+            return new RankTest(rank1).RankSet();
+        } else if (rank > 2999) {
+            rank1 = Rank.Diamond;
+            return new RankTest(rank1).RankSet();
+        } else if (rank > 2499) {
+            rank1 = Rank.Platinum;
+            return new RankTest(rank1).RankSet();
+        } else if (rank > 1999) {
+            rank1 = Rank.Gold;
+            return new RankTest(rank1).RankSet();
+        } else if (rank > 1499) {
+            rank1 = Rank.Silver;
+            return new RankTest(rank1).RankSet();
+        } else {
+            rank1 = Rank.Bronze;
+            return new RankTest(rank1).RankSet();
         }
-        else if (rank > 3499){
-            return "Master";
+    }
+
+    public enum Rank {Grandmaster, Master, Diamond, Platinum, Gold, Silver, Bronze}
+
+    public static class RankTest {
+        Rank rank;
+
+        public RankTest(Rank rank) {
+            this.rank = rank;
         }
-        else if (rank > 2999){
-            return "Diamond";
-        }
-        else if (rank > 2499){
-            return "Platinum";
-        }
-        else if (rank > 1999){
-            return "Gold";
-        }
-        else if (rank > 1499){
-            return "Silver";
-        }
-        else{
-            return "Bronze";
+
+        public String RankSet() {
+            String rankName = null;
+            switch (rank) {
+                case Grandmaster -> rankName = "Grandmaster";
+                case Master -> rankName = "Master";
+                case Diamond -> rankName = "Diamond";
+                case Platinum -> rankName = "Platinum";
+                case Gold -> rankName = "Gold";
+                case Silver -> rankName = "Silver";
+                case Bronze -> rankName = "Bronze";
+            }
+            return rankName;
         }
     }
 }
