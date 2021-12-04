9c8aaf95b0fcd92ae6dc7b1b1fc3501802dbbbb5
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-12-04 13:02:29 -0500

    Refactor naming in DataFormatter class

--- src/main/java/edu/bsu/cs/Model/DataFormatter.java
+++ src/main/java/edu/bsu/cs/Model/DataFormatter.java
@@ -7,38 +7,38 @@ public class DataFormatter {
 
         if (skillRating > 3999) {
             rank = Rank.Grandmaster;
-            return new RankTest(rank).RankSet();
+            return new RankModifier(rank).accessRankName();
         } else if (skillRating > 3499) {
             rank = Rank.Master;
-            return new RankTest(rank).RankSet();
+            return new RankModifier(rank).accessRankName();
         } else if (skillRating > 2999) {
             rank = Rank.Diamond;
-            return new RankTest(rank).RankSet();
+            return new RankModifier(rank).accessRankName();
         } else if (skillRating > 2499) {
             rank = Rank.Platinum;
-            return new RankTest(rank).RankSet();
+            return new RankModifier(rank).accessRankName();
         } else if (skillRating > 1999) {
             rank = Rank.Gold;
-            return new RankTest(rank).RankSet();
+            return new RankModifier(rank).accessRankName();
         } else if (skillRating > 1499) {
             rank = Rank.Silver;
-            return new RankTest(rank).RankSet();
+            return new RankModifier(rank).accessRankName();
         } else {
             rank = Rank.Bronze;
-            return new RankTest(rank).RankSet();
+            return new RankModifier(rank).accessRankName();
         }
     }
 
     public enum Rank {Grandmaster, Master, Diamond, Platinum, Gold, Silver, Bronze}
 
-    public static class RankTest {
+    public static class RankModifier {
         Rank rank;
 
-        public RankTest(Rank rank) {
+        public RankModifier(Rank rank) {
             this.rank = rank;
         }
 
-        public String RankSet() {
+        public String accessRankName() {
             String rankName = null;
             switch (rank) {
                 case Grandmaster -> rankName = "Grandmaster";
