d9eb4c539e85a291875bdd86bf7b3400071b5cda
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2021-12-12 16:49:13 -0500

    Add criticalHitsAccuracy, scopedAccuracy, and timePlayed to each hero's stats box

--- src/main/java/edu/bsu/cs/View/HeroStatsBox.java
+++ src/main/java/edu/bsu/cs/View/HeroStatsBox.java
@@ -3,7 +3,7 @@ package edu.bsu.cs.View;
 import edu.bsu.cs.Model.Hero;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -12,27 +12,43 @@ import java.util.List;
 public class HeroStatsBox extends VBox {
     private final Label heroName = new Label("");
     private final Label weaponAccuracy = new Label("");
+    private final Text criticalHitsAccuracy = new Text("");
+    private final Text scopedAccuracy = new Text("");
+    private final Text eliminationsPerLife = new Text("");
     private final Label winPercentage = new Label("");
     private final Label gamesWon = new Label("");
-    private final Text eliminationsPerLife = new Text("");
+    private final Text timePlayed = new Text("");
     private final Font font = new Font(20);
 
     public HeroStatsBox() {
         setAlignment(Pos.CENTER);
-        getChildren().addAll(heroName, weaponAccuracy, winPercentage, gamesWon, eliminationsPerLife);
+        getChildren().addAll(heroName, weaponAccuracy, criticalHitsAccuracy, scopedAccuracy,
+                winPercentage, gamesWon, eliminationsPerLife, timePlayed);
     }
 
     public void updateHeroInfo(int index, List<Hero> heroList) {
         setVisible(true);
-        heroName.setText(heroList.get(index).accessHeroName());
+        heroName.setText(reformatText(heroList.get(index).accessHeroName()));
         heroName.setFont(font);
-        weaponAccuracy.setText(heroList.get(index).accessWeaponAccuracy());
+        weaponAccuracy.setText(reformatText(heroList.get(index).accessWeaponAccuracy()));
         weaponAccuracy.setFont(font);
-        winPercentage.setText(heroList.get(index).accessWinPercentage());
+        winPercentage.setText(reformatText(heroList.get(index).accessWinPercentage()));
         winPercentage.setFont(font);
-        gamesWon.setText(heroList.get(index).accessGamesWon());
+        gamesWon.setText(reformatText(heroList.get(index).accessGamesWon()));
         gamesWon.setFont(font);
-        eliminationsPerLife.setText(heroList.get(index).accessEliminationsPerLife());
+        criticalHitsAccuracy.setText(reformatText(heroList.get(index).accessCriticalHitAccuracy()));
+        criticalHitsAccuracy.setFont(font);
+        scopedAccuracy.setText(reformatText(heroList.get(index).accessScopedAccuracy()));
+        scopedAccuracy.setFont(font);
+        timePlayed.setText(reformatText(heroList.get(index).accessTimePlayed()));
+        timePlayed.setFont(font);
+        eliminationsPerLife.setText(reformatText(heroList.get(index).accessEliminationsPerLife()));
         eliminationsPerLife.setFont(font);
+    }
+
+    private String reformatText(String text) {
+        String reformattedText = text.replace("=", " = ").trim();
+        reformattedText = reformattedText.substring(0,1).toUpperCase() + reformattedText.substring(1);
+        return reformattedText;
     }
 }
